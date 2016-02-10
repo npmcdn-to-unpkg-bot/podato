1588c9df667464cad7e18586d55c1bbc27ec09e0
Frederik Creemers <frederikcreemers@gmail.com>
2016-02-10 11:50:38 +0100

    Set up webpack for compiling frontend resources.

---
+++ frontend/graphql.entry.jsx
@@ -0,0 +1,12 @@
+var GraphiQL = require("graphiql");
+var ReactDOM = require("react-dom");
+
+function graphQLFetcher(graphQLParams) {
+    return fetch(window.location.origin + '/graphql', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(graphQLParams),
+    }).then(response => response.json());
+}
+
+ReactDOM.render(<GraphiQL fetcher={graphQLFetcher}></GraphiQL>, document.body);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+'use strict';
+var webpack = require("webpack")
+var SaveAssetsJson = require("assets-webpack-plugin");
+var path = require("path");
+
+module.exports = {
+    target: "web",
+    devtool: "#source-map",
+    bail: true,
+    entry: {
+        graphiql: "./frontend/graphql.entry.jsx"
+    },
+    output: {
+        path: path.join(__dirname, "main/static/"),
+        filename: "[name].bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel', // 'babel-loader' is also a legal name to reference
+                query: {
+                    presets: ['react', 'es2015'],
+                    cacheDirectory: true
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(true),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            output: {
+                comments: false
+            },
+            compress: {
+                warnings: true,
+                screw_ie8: true
+            }
+        }),
+        new webpack.ProvidePlugin({
+            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        })
+    ]
+}
