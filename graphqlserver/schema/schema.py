5b1f758f2a71d5439380bcd35d8fff9f30dc92a0
Frederik Creemers <frederikcreemers@gmail.com>
2016-02-11 18:45:34 +0100

    use gevent graphql executor.

--- graphqlserver/schema/schema.py
+++ graphqlserver/schema/schema.py
@@ -1,10 +1,11 @@
 from graphene import relay, ObjectType, Schema
-
+from graphql.core.execution.middlewares.gevent import GeventExecutionMiddleware
+from graphql.core.execution import Executor
 from users import UserQuery
 
 class Query(UserQuery):
     pass
 
 
-schema = Schema(name="Podato Schema")
+schema = Schema(name="Podato Schema", executor=Executor([GeventExecutionMiddleware()]))
 schema.query = Query
