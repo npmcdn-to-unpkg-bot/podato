cb7124c006d381311d3dd5f7d74c3e29baba8a14
Frederik Creemers <frederikcreemers@gmail.com>
2016-02-13 14:24:52 +0100

    migrations

--- podcasts/models.py
+++ podcasts/models.py
@@ -7,9 +7,12 @@ from utils.fields import SeparatedValuesField
 
 class Category(models.Model):
     """Podcast categories"""
-    name = models.CharField(max_length=40, primary_key=True)
-    parent_name = models.CharField(max_length=40, primary_key=True)
+    name = models.CharField(max_length=40)
+    parent_name = models.CharField(max_length=40, blank=True)
     visible = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ["name", "parent_name"]
 
     def __str__(self):
         return self.name
@@ -27,17 +30,17 @@ class Podcast(models.Model):
     url = models.CharField(max_length=255, primary_key=True)
     link = models.CharField(max_length=255)
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200, blank=True)
+    author = models.CharField(max_length=200, blank=True, null=True)
     description = models.TextField()
-    copyright = models.CharField(max_length=200, blank=True)
-    image = models.CharField(max_length=255, blank=True)
+    copyright = models.CharField(max_length=200, blank=True, null=True)
+    image = models.URLField(max_length=255, blank=True, null=True)
     explicit = models.CharField(max_length=1,default="u", choices=[("u", "not set",),("c", "clean"), ("e", "explicit")])
-    owner_email = models.CharField(max_length=200)
-    owner_name = models.CharField(max_length=200)
-    tags = SeparatedValuesField(max_length=200)
+    owner_email = models.CharField(max_length=200, blank=True, null=True)
+    owner_name = models.CharField(max_length=200, blank=True, null=True)
+    tags = SeparatedValuesField(max_length=200, blank=True, null=True)
     categories = models.ManyToManyField(Category)
     last_fetched = models.DateTimeField()
-    warnings = SeparatedValuesField(max_length=200)
+    warnings = SeparatedValuesField(max_length=200, blank=True, null=True)
 
     objects = PodcastManager()
 
@@ -48,21 +51,21 @@ class Podcast(models.Model):
 class Episode(models.Model):
     """Represents a podcast episode."""
     guid = models.CharField(max_length=400, primary_key=True)
-    podcast = models.ForeignKey(Podcast, null=False)
+    podcast = models.ForeignKey(Podcast, null=True)
     title = models.CharField(max_length=255)
-    link = models.CharField(max_length=255)
-    subtitle = models.CharField(max_length=255)
-    summary = models.CharField(max_length=500)
-    description = models.TextField()
-    content = models.TextField()
+    link = models.URLField(max_length=255)
+    subtitle = models.CharField(max_length=255, blank=True, null=True)
+    summary = models.CharField(max_length=500, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
     enclosure_type = models.CharField(max_length=20)
-    enclosure_url = models.CharField(max_length=255)
+    enclosure_url = models.URLField(max_length=255)
     duration = models.PositiveIntegerField(default=0)
-    explicit = models.CharField(max_length="u", choices=[("u", "not set",),("c", "clean"), ("e", "explicit")])
-    author = models.CharField(max_length=255)
-    image = models.CharField(max_length=255)
+    explicit = models.CharField(max_length=1, default="u", choices=[("u", "not set",),("c", "clean"), ("e", "explicit")])
+    author = models.CharField(max_length=255, blank=True, null=True)
+    image = models.URLField(max_length=255, blank=True, null=True)
     published = models.DateTimeField()
-    warnings = SeparatedValuesField(max_length=200)
+    warnings = SeparatedValuesField(max_length=200, blank=True)
 
     def __str__(self):
         return self.title
@@ -75,7 +78,7 @@ class Subscription(models.Model):
     subscription isn't deleted, but rather, the unssubscribed field is set. So to get a list of all podcasts the user is
     currently subscribed to, get all subscriptions whose unsubscribed field is None."""
 
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=False, related_name="subscriptions")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, related_name="subscriptions")
     podcast = models.OneToOneField(Podcast, related_name="subscriptions")
     subscribed = models.DateTimeField(auto_now_add=True, help_text="The date on which the user subscribed to this podcast")
     unsubscribed = models.DateTimeField(help_text="The date on which the user unsubscribed from this podcast.")
