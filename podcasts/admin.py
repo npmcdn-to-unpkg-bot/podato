830ae0a545223c98c72cadc3025631bbd9d0d8a5
Frederik Creemers <frederikcreemers@gmail.com>
2016-02-13 17:09:55 +0100

    show podcast models in the admin ui

--- podcasts/admin.py
+++ podcasts/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from podcasts.models import Podcast, Episode, Category
 # Register your models here.
 
-admin.register(Category)
-admin.register(Podcast)
-admin.register(Episode)
+admin.site.register(Category)
+admin.site.register(Podcast)
+admin.site.register(Episode)
