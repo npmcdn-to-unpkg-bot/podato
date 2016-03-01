6b19ca5418f2b7acaa2ba8c01c149fe5d2633c2c
Frederik Creemers <frederikcreemers@gmail.com>
2016-03-01 13:12:48 +0100

    fix logout url regex.

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ from main import views
 urlpatterns = [
     url(r'^confirmation_email$', views.email_confirmation_sent, name="confirmation_email"),
     url(r'^oauth_callback$', views.oauth_callback, name="oauth_callback"),
-    url(r'^/logout', views.logout_user),
+    url(r'^logout', views.logout_user),
     url(r'^.*$', views.index, name='index')
 ]
